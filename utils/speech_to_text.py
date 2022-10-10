4667c13289f0e74b245f8b9b1dfef5b419e02ba6
Sahilvohra58 <70532930+Sahilvohra58@users.noreply.github.com>
2022-10-10 15:59:14 -0400

    Added logging

--- utils/speech_to_text.py
+++ utils/speech_to_text.py
@@ -5,6 +5,7 @@ from typing import Union, Optional
 import moviepy.editor as mp
 import pandas as pd
 from google.cloud import speech
+from pathlib import Path
 
 from config.config import LoadConfigs
 
@@ -18,28 +19,28 @@ class SpeechToText():
 
         self.service_account_path = os.path.join(
                     cfg.UPLOAD_DIRECTORY, "service_account", "service_account.json")
-        
-       
 
     def connect_google_client(self, service_account_json: Union[dict, str], return_speech_client=False):
         os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = service_account_json
         try:
             self._speech_client = speech.SpeechClient()
-            if return_speech_client:
+            logging.debug(f"Speech client successfully connected") 
+            if return_speech_client:  
                 return self._speech_client 
             else:
                 return True
-        except:
+        except Exception as E:
+            logging.error("Failed to connect to gcloud console."
+                        f"Check your internet connection or try again later - ERROR : {E}") 
             return False
-            #raise ConnectionAbortedError("Failed to connect to gcloud console. 
-            # Check your internet connection or try again later")
     
     def _api_request(self, audio_file_path: str):
         with open(audio_file_path, 'rb') as f1:
-                byte_data_mp3 = f1.read()
-
+            logging.debug("Reading audio file")
+            byte_data_mp3 = f1.read()
+          
         audio_mp3 = speech.RecognitionAudio(content=byte_data_mp3)
-
+    
         self._speech_client = self.connect_google_client(self.service_account_path, return_speech_client=True)
         
         config_mp3 = speech.RecognitionConfig(
@@ -49,14 +50,17 @@ class SpeechToText():
                 language_code=cfg.LANGUAGE_CODE
             )
 
+        logging.debug("Sending audio for Speech recognition")
         response_standard_mp3 = self._speech_client.recognize(
                                     config=config_mp3,
                                     audio=audio_mp3
                                     )
+        logging.debug("Speech recognitionb successful")
         
         return response_standard_mp3.results
     
     def _process_transcript(self, results) -> pd.DataFrame:
+        logging.debug("Processing received results in a Pandas DF")
         transcript_data = pd.DataFrame(columns=['Content', 'Start_Time', 'End_Time'])
         words, start_time_with_count, end_time_with_count, count = ("", 0,0,0)
 
@@ -86,25 +90,13 @@ class SpeechToText():
 
         append_list = [words, start_time_with_count.total_seconds(), self._my_clip.duration] #end_time.total_seconds()]
         transcript_data.loc[len(transcript_data)] = append_list
-
+        logging.debug("Processing successful")
         return transcript_data
     
-    def populate_data(transcript_dataframe: pd.DataFrame, Transcript_Data_Class, db):
-        transcript_db_path = './transcript.db'
-        if os.path.exists(transcript_db_path):
-            os.remove(transcript_db_path)
-        db.create_all()
-        for _, row in transcript_dataframe.iterrows():
-            append_data = Transcript_Data_Class(
-                content = row["Content"],
-                start_time = float(row["Start_Time"]),
-                end_time = float(row['End_Time']),
-            )
-            db.session.add(append_data)
-            db.session.commit()
-
+    
     def generate_subtitles(self, transcript_file_name: Optional[str] = None):
         self._my_clip = mp.VideoFileClip(self.video_file_path)
+        logging.debug("Loaded video clip for generating subtitle")
         if transcript_file_name:
             extension = transcript_file_name.split(".")[-1]
             if extension == "db":
@@ -134,8 +126,6 @@ class SpeechToText():
                 logging.debug(f"Writing {pth} file into transcript.db")    
         
         else:
-            
-
             temp_audio_file_path = os.path.join(
                 cfg.TEMP_FILES_FOLDER, 
                  "audio.mp3")
@@ -148,25 +138,29 @@ class SpeechToText():
             logging.debug("Requesting API")
             results = self._api_request(audio_file_path=temp_audio_file_path)
 
-            logging.debug("Received response. Processing response")
             transcript = self._process_transcript(results=results)
-            logging.debug("Data processed")
         
+        os.makedirs(name=cfg.TEMP_FILES_FOLDER, exist_ok=True)
         temp_transcript_file_path = os.path.join(
             cfg.TEMP_FILES_FOLDER, 
             "transcript.csv")
+        logging.debug(f"Writing transcript in a csv file at {temp_transcript_file_path}")
         transcript.to_csv(temp_transcript_file_path, index=False)
 
         temp_audio_file_path = os.path.join(
             cfg.TEMP_FILES_FOLDER, 
             "audio.mp3")
         if not os.path.exists(temp_audio_file_path):
+            logging.debug(f"Writing audio in a mp3 file at {temp_audio_file_path}")
             self._my_clip.audio.write_audiofile(temp_audio_file_path)
 
         return transcript
 
     def delete_temp_file(self):
+        logging.debug(f"Deleting {cfg.TEMP_FILES_FOLDER}")
         shutil.rmtree(cfg.TEMP_FILES_FOLDER, ignore_errors=True)
+        logging.debug(f"Deleting {cfg.UPLOAD_DIRECTORY}")
         shutil.rmtree(cfg.UPLOAD_DIRECTORY, ignore_errors=True)
         if os.path.exists('transcript.db'):
-                os.remove('transcript.db')
+            logging.debug(f"Deleting transcript.db")
+            os.remove('transcript.db')
