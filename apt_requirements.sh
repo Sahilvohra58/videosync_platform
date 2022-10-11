5af9d6382a315443b94847382ba9bad8805fc1d8
Sahilvohra58 <70532930+Sahilvohra58@users.noreply.github.com>
2022-10-10 20:55:25 -0400

    Update apt_requirements.sh

--- apt_requirements.sh
+++ apt_requirements.sh
@@ -1,5 +1,5 @@
 apt-get -y update
-apt-get install -y ffmpeg=3.4.11-0ubuntu0.1
+apt-get install -y ffmpeg
 #=7:4.3.4-0+deb11u1
-apt-get install -y imagemagick=8:6.9.7.4+dfsg-16ubuntu6.13
+apt-get install -y imagemagick
 #=8:6.9.11.60+dfsg-1.3
